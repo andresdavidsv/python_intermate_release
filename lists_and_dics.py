33d715862db3597dcfc7606f9f231f2e2ed57d81
Andres David Solarte Vidal <andresdavidsolartevidal@gmail.com>
2021-06-15 11:55:42 -0500

    :tada: Project of pyton level mid

---
+++ lists_and_dics.py
@@ -0,0 +1,24 @@
+def run():
+  my_list = [1,"hello",True,4.5]
+  my_dict = {"firstname":"Andres","lastname":"Solarte"}
+
+  super_list=[
+    {"firstname":"Andres","lastname":"Solarte"},
+    {"firstname":"David","lastname":"Vidal"},
+    {"firstname":"Maria","lastname":"Solarte"},
+    {"firstname":"Paz","lastname":"Cruz"},
+  ]
+  super_dict ={
+    "natural_nums":[1,2,3,4,5],
+    "integer_nums": [-1,-2,0,1,2,3],
+    "floating_nums":[1.1,4.5,6.43]
+  }
+
+  for key,value in super_dict.items():
+    print(key,"-",value)
+
+  for i in super_list:
+    print(i)
+
+if __name__=='__main__':
+  run()
